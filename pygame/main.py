16bcf8d99f508177948e18b8ac378090a7de136f
John Doe <johndoe@example.com>
2022-07-07 20:01:57 +0700

    Ok

--- pygame/main.py
+++ pygame/main.py
@@ -4,20 +4,37 @@ import sys
 from object import *
 import random 
 
+class Music:
+    def __init__(self):
+        pygame.mixer.init()
+        pygame.mixer.music.load('pygame\\res\\theme.mp3')
+        pygame.mixer.music.play(loops=True)
+
 class Logic:
     def __init__(self, screen, speed):
         self.player = Player(screen)
         self.platform = Platform(screen)
         self.screen = screen
         self.speed = speed
+        self.game_over = False
+        self.retry_button = self.get_retry_button()
+        self.retry_button_rect = False
+        self.score = 0
+        self.count_score = True
+        # self.music = Music()
         
     def on_update(self):
+        if(self.game_over):
+            self.render_game_over()
+            return
         self.player.render()
         self.rener_platforms()
         self.move_platforms()
         self.try_destroy_platform()
         self.try_create_platform()
-
+        self.count()
+        self.check_game_over()
+        
     def rener_platforms(self):
         for platform in ObjectEngine.all_objects:
             platform.render()
@@ -41,27 +58,72 @@ class Logic:
     def generate_platform(self):
         new_platform = Platform(self.screen)
         min_y = 250
-        max_y = 500
-        min_bias_X = 100
-        max_bias_X = 150
+        max_y = 450
+        min_bias_X = 180
+        max_bias_X = 200
         new_platform.posY = random.uniform(min_y, max_y)
         new_platform.posX = ObjectEngine.all_objects[-2].posX + random.uniform(min_bias_X, max_bias_X)
         return new_platform
 
+    def count(self):
+        if self.player.is_grounded:
+            if self.count_score:
+                self.score += 1
+                self.count_score = False
+        else:
+            self.count_score = True
+        
+        font_c = pygame.font.SysFont("Arial", 20)
+        score_text = font_c.render(str(self.score), True, (255,255,255))
+        self.screen.blit(score_text, (0, 0))
+        
+       
+            
+    def check_game_over(self): 
+        if self.player.posY >= 600:
+            self.game_over = True 
+            ObjectEngine.clear_all_objects()
+            
+    def render_game_over(self):
+        self.check_game_over()        
+        self.retry_button_rect = self.render_button()
+    
+    def render_button(self):
+        w = self.retry_button.get_width() / 2
+        h = self.retry_button.get_height() / 2
+        return self.screen.blit(self.retry_button, (400 - w, 300 - h))
+        
+    def get_retry_button(self):
+        font = pygame.font.SysFont("Arial", 50)
+        return font.render("Заново", True, (255,255,255))
+            
+def start_game(screen):
+    return Logic(screen, 2.5)        
 def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    logic = Logic(screen, 1)
+    logic = start_game(screen)
     pygame.display.set_caption('Square')
     background_color = (0, 0, 0)
     run = True
     clock = pygame.time.Clock()
-
+    #b1 = Logic.buttons(screen, (400, 300), "Продолжить игру")
+    #b2 = Logic.buttons(screen, (500, 300), "Начать заново")
+    
     while run:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     logic.player.jump()
+                    logic.player.jump_button_pressed = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    logic.player.jump_button_pressed = False
+                    logic.player.in_jump = False
+            if event.type == pygame.MOUSEBUTTONDOWN and logic.retry_button_rect:
+                x, y = event.pos
+                if logic.retry_button_rect.collidepoint(x ,y):
+                    logic = start_game(screen)
             if event.type == pygame.QUIT:
                 run = False
                 sys.exit()
--- pygame/object.py
+++ pygame/object.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 class ObjectEngine:
     all_objects = []
 
@@ -8,7 +7,14 @@ class ObjectEngine:
     def add_object(obj):
         ObjectEngine.all_objects.append(obj)
 
-    def check_collision(player, on_collision):
+    @staticmethod
+    def clear_all_objects():
+        ObjectEngine.all_objects = []
+
+    def check_collision(player, on_collision): 
+
+        if ObjectEngine.check_collision_right(player, player.on_collision_right):
+            return False
 
         def is_collide(platform):
             player_left_bot = {'x': player.posX, 'y': player.posY + player.h}
@@ -19,15 +25,41 @@ class ObjectEngine:
             platform_right_top = {'x': platform.posX +
                                   platform.w, 'y': platform.posY}
 
-            return (player_left_bot['y'] >= platform_left_top['y'] and (player_right_bot['x'] >= platform_left_top['x'] and player_left_bot['x'] <= platform_right_top['x']), player_left_bot['y'] != platform_left_top['y'], platform_left_top['y'])
+            return (player_left_bot['y'] >= platform_left_top['y'] and (player_right_bot['x'] >= platform_left_top['x'] and player_left_bot['x'] <= platform_right_top['x']), 
+                    player_left_bot['y'] != platform_left_top['y'], 
+                    platform_left_top['y'])
 
+            
+        
         for platform in ObjectEngine.all_objects:
             is_collide_check = is_collide(platform)
             if is_collide_check[0]:
                 on_collision(is_collide_check[1], is_collide_check[2])
+                
+    def check_collision_right(player, on_collision): 
 
+        def is_collide(platform):
+            player_left_bot = {'x': player.posX, 'y': player.posY + player.h}
+            player_right_bot = {'x': player.posX +
+                                player.w, 'y': player.posY + player.h}
 
-class GameObject:
+            platform_left_top = {'x': platform.posX, 'y': platform.posY}
+            platform_right_top = {'x': platform.posX +
+                                  platform.w, 'y': platform.posY}
+
+            return (player_right_bot['y'] > platform_left_top['y'] + 30 and player_right_bot['x'] >= platform_left_top['x'] and player_left_bot['x'] <= platform_right_top['x'], 
+                    player_right_bot['x'] != platform_left_top['x'], 
+                    platform_left_top['x'])
+
+            
+        
+        for platform in ObjectEngine.all_objects:
+            is_collide_check = is_collide(platform)
+            if is_collide_check[0]:
+                on_collision(is_collide_check[1], is_collide_check[2])
+            
+
+class GameObject(pygame.Surface): 
     def __init__(self, screen):
         self.posX = 0
         self.posY = 0
@@ -35,10 +67,12 @@ class GameObject:
         self.h = 0
         self.color = (0, 0, 0)
         self.screen = screen
+        
 
     def render(self):
-        pygame.draw.rect(self.screen, self.color,
+        res = pygame.draw.rect(self.screen, self.color,
                          (self.posX, self.posY, self.w, self.h))
+        return res
  
     def destroy(self):
         ObjectEngine.all_objects.remove(self)
@@ -50,17 +84,18 @@ class Player(GameObject):
         self.color = (255, 255, 0)
         self.h = 50
         self.w = 50
-        self.posX = 400 - (self.w / 2)
-        self.posY = 20
+        self.posX = 400 - (self.w / 2) - 50
+        self.posY = 200
 
         self.is_grounded = False
         self.g = 10
 
-        self.jump_force = 200
-        self.jump_speed = 15 + self.g
+        self.jump_force = 300
+        self.jump_speed = 17 + self.g
         self.in_jump = False
+        self.jump_button_pressed = False
 
-    def jump(self):
+    def jump(self): 
         if not self.is_grounded:
             return
         self.in_jump = True
@@ -72,17 +107,18 @@ class Player(GameObject):
         if (isLower):
             self.posY = platform_top - self.h
 
+    def on_collision_right(self, isRighter, platform_left):
+        self.posX -= 3.5
     def render(self):
-        super().render()
+        self.player_sprite = super().render()
         self.is_grounded = False
         ObjectEngine.check_collision(self, self.on_collision)
         if not self.is_grounded:
             self.posY += self.g
-        if self.in_jump:
+        if self.in_jump and self.jump_button_pressed:
             if self.posY <= self.jump_end:
                 self.in_jump = False
             self.posY -= self.jump_speed
-
 
 class Platform(GameObject):
     def __init__(self, screen):
